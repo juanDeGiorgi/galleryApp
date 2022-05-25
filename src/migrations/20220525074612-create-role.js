6acb990434afe39079d7142655585075e8b95f24
Ezequiel Rango <ezequielrango29@gmail.com>
2022-05-25 05:22:59 -0300

    create model and migration role

---
+++ src/migrations/20220525074612-create-role.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Roles', {
+      id : {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      name:{
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      description:{
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        allowNull: true,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Roles');
+  }
+};
