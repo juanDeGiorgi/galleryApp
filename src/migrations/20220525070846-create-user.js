40f748274ee0ea2a2c2a4b3eb8f382b319bdfe9d
Ezequiel Rango <ezequielrango29@gmail.com>
2022-05-25 05:20:57 -0300

    create model and migration users

---
+++ src/migrations/20220525070846-create-user.js
@@ -0,0 +1,57 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id : {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull : false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull : false
+      },
+      image:{
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      password:{
+        type: Sequelize.STRING,
+        allowNull : false
+      },
+      lastLogin:{
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      role_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Role',
+          key: 'id'
+        },
+        onUpdate : 'CASCADE',
+        onDelete : 'CASCADE',
+      }, 
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        allowNull: true,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
